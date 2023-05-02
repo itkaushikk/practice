0648d3cf7983ac2888973648a8c3b71aded54a91
kaushik <itkaushikk@gmail.com>
2023-05-02 18:18:35 +0530

    formatted the code using black extension in vsc

--- The Big Book of Small Python Projects/prj1_bagels.py
+++ The Big Book of Small Python Projects/prj1_bagels.py
@@ -1,6 +1,7 @@
 import random
 
-print('''Bagels, a deductive logic game.
+print(
+    """Bagels, a deductive logic game.
 By Al Sweigart al@inventwithpython.com
 I am thinking of a 3-digit number. Try to guess what it is.
 Here are some clues:
@@ -9,7 +10,8 @@ Pico One digit is correct but in the wrong position.
 Fermi One digit is correct and in the right position.
 Bagels No digit is correct.
 I have thought up a number.
-You have 10 guesses to get it.''')
+You have 10 guesses to get it."""
+)
 
 guessed_no = str(random.randint(100, 999))
 
@@ -17,7 +19,7 @@ count = 0
 while True:
     print(f"Guess #{count+1}")
     guessed_user = input("> ")
-    
+
     for index_user in range(len(guessed_user)):
         if guessed_user[index_user] == guessed_no[index_user]:
             print("Fermi", end=" ")
@@ -25,7 +27,7 @@ while True:
             print("Pico", end=" ")
         else:
             print("Bagels", end=" ")
-    
+
     print("")
     count += 1
     if count == 10:
